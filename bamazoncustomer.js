15d3a995f4ed6189b90370313596622120d383ac
APabloJ <altisimop@gmail.com>
2018-09-24 21:58:17 -0500

    added the questionaire using npm package inquirer. Running the questions(); function prompts the user to specify an item and how many units they want. the same function makes the proper adjustments to the DB subtracting the number of units the user wanted from the quantity of that item available. Ends with a console.log of the transaction.

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -26,9 +26,61 @@ function display() {
             console.log("Product ID: " + results[i].item_id + "  Product Name: " + results[i].product_name + "  Price: $" + results[i].price + " Department: " + results[i].department_name + " Stock Quantity: " + results[i].stock_quantity);
             console.log("----------------------------------------------------------------------------------");
         }
-
+        //questions function called which prompts user questions and takes order/updates inventory
+        questions();
     });
 };
+
+
+
+
+//questions function will prompt user questions and update MySQL DB given the user input. Detailed below.
+function questions() {
+    //Prompts user questions
+    inquirer.prompt([{
+        name: "product",
+        type: "input",
+        message: "What is the ID of the product you would like to buy?"
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "How many of this item do you want?"
+    }]).then(function (answer) {
+
+        let idChosen = answer.product;
+        let quantityChosen = answer.quantity;
+
+        //select product name, price, and stock quantity from the products table for the specific item id that was chosen.
+        connection.query('SELECT product_name, price, stock_quantity FROM products WHERE item_id =' + idChosen,
+            function (error, results) {
+
+                // newQuantity is set to equal to the current inventory amount minus the quantity of item the user selects
+                let newQuantity = results[0].stock_quantity - quantityChosen;
+                //if stock_quantity is less what than what the user wants to buy, console.log that there isnt enough inventory. 
+                if (results[0].stock_quantity < quantityChosen) {
+                    console.log("There is not enough of this item to fulfill your order.")
+                    questions();
+
+                } else {
+                    //if the amount of items is available to buy, the products table will update the quantity to reflect your purchase
+                    connection.query("UPDATE products SET? WHERE?", [{
+                        stock_quantity: newQuantity
+                    }, {
+                        item_id: idChosen
+                    }]);
+                    //console logs which will display the total cost of your order given the user input for quantity of items purchased and name of product purchased
+                    if (quantityChosen === "1") {
+                        console.log("Your total is: $" + results[0].price * quantityChosen + ". You have purchased " + quantityChosen + " " + results[0].product_name);
+
+                    } else {
+                        console.log("Your total is: $" + results[0].price * quantityChosen + ". You have purchased " + quantityChosen + " " + results[0].product_name + "s");
+
+                    }
+                }
+            });
+    });
+}
+
 
 
 
