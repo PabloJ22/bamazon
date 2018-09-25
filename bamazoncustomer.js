4629c1fa05b2a76d64b5f503c347cc93b2417d1e
APabloJ <altisimop@gmail.com>
2018-09-24 21:52:05 -0500

    added the display(); funcion that displays the data in the MySQL DB.

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -11,3 +11,26 @@ let connection = mysql.createConnection({
     password: "rooot1",
     database: "bamazon_db"
 });
+
+
+
+//the display function is used to pull information from my sql table.
+function display() {
+    connection.query('SELECT item_id, product_name, price, department_name, stock_quantity FROM products', function (err, results) {
+        console.log("Here is a list of all merchandise available in our store!");
+        console.log("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/");
+
+        //loop used to cycle through all items in my inventory
+        for (let i = 0; i < results.length; i++) {
+            //selected Items will be returned in the console.log
+            console.log("Product ID: " + results[i].item_id + "  Product Name: " + results[i].product_name + "  Price: $" + results[i].price + " Department: " + results[i].department_name + " Stock Quantity: " + results[i].stock_quantity);
+            console.log("----------------------------------------------------------------------------------");
+        }
+
+    });
+};
+
+
+
+//display function called to initialize app
+display();
